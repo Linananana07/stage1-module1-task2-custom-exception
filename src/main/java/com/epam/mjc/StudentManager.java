8697fb28d859b5bd610cd1b8e9267dec9fd68905
lina Yuldasheva <linademyanenko7@gmail.com>
2024-09-03 20:23:39 +0600

    update class StudentManager

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,6 +6,16 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
+    boolean findStudent = false;
+    for (long id : IDs) {
+      if (studentID == id) {
+        findStudent = true;
+        break;
+      }
+    }
+    if (!findStudent) {
+      throw new ExceptionStudentId(studentID);
+    }
     return Student.getValueOf(studentID);
   }
 
